3b25c4e7a9a773c9abfdb390f3e797ac8a8bd188
binit <binitgupta.1711@gmail.com>
2025-10-24 00:58:02 +0530

    Color added to the banner elemetn

--- src/banner.c
+++ src/banner.c
@@ -1,9 +1,18 @@
 #include <stdio.h>
 #include "banner.h"
 
+// --- Define colors  ---
+#define KNRM  "\x1B[0m"   // Reset
+#define KBLU  "\x1B[34m"  // Blue
+#define KGRN  "\x1B[32m"  // Green
+#define KCYN  "\x1B[36m"  // Cyan
+#define KYEL  "\x1B[33m"  // Yellow
+#define KGRY  "\x1B[90m"  // Dark Gray
+
 void print_banner(void) {
-    printf("\n");
-    printf("\n");
+    printf("\n\n\n");
+    // --- Print the main banner in blue ---
+    printf(KBLU); 
     printf(" /$$$$$$$                          /$$                /$$$$$$  /$$                 /$$ /$$          /$$$  \n");
     printf("| $$__  $$                        | $$               /$$__  $$| $$                | $$| $$         |_  $$ \n");
     printf("| $$  \\ $$ /$$   /$$ /$$$$$$/$$$$ | $$$$$$$         | $$  \\__/| $$$$$$$   /$$$$$$ | $$| $$       /$$ \\  $$\n");
@@ -12,5 +21,11 @@ void print_banner(void) {
     printf("| $$  | $$| $$  | $$| $$ | $$ | $$| $$  | $$         /$$  \\ $$| $$  | $$| $$_____/| $$| $$       /$$  /$$/\n");
     printf("| $$$$$$$/|  $$$$$$/| $$ | $$ | $$| $$$$$$$/        |  $$$$$$/| $$  | $$|  $$$$$$$| $$| $$      |__//$$$/ \n");
     printf("|_______/  \\______/ |__/ |__/ |__/|_______/          \\______/ |__/  |__/ \\_______/|__/|__/         |___/  \n");
-    printf("\nWelcome to dumb-shell! (made by a dumb developer) Type 'help' for commands.\n\n\n");
+    
+    // --- Reset to normal, then print the sub-heading in a different color ---
+    printf(KNRM "\n"); // Reset color
+    printf(KYEL "Welcome to dumb-shell!" KNRM); // Yellow for the title
+    printf(KGRY " (made by a dumb developer) " KNRM); // Gray for the joke
+    printf("Type 'help' for commands.\n\n");
+    printf(KNRM); // Final reset, just in case
 }
