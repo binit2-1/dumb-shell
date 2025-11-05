188c6baafc552f741ac67a4dcefdad5f56ad59bc
binit <binitgupta.1711@gmail.com>
2025-11-05 22:52:14 +0530

    Update banner and help command messages for improved clarity and humor

--- src/builtins.c
+++ src/builtins.c
@@ -33,25 +33,24 @@ int dsh_help(char **args) {
     (void)args; // Silences the "unused variable" warning
 
     printf("\n");
-    printf("You're using dumb-shell and you need... `help`?\n");
-    printf("Alright, fine. I guess I can't expect everyone to be a genius...\n\n");
-
-    printf("This shell was (allegedly) \"made by a dumb developer,\" so let's keep it simple.\n");
+    printf("Ugh. You're using **barely-alive shell**... and you actually need `help`?\n");
+    printf("Sigh. Fine. I'll stop... *not*... working for a second to explain.\n");
+    printf("Honestly, I'm as surprised as you are that this even launched.\n");
     printf("\n");
     
-    printf("--- My Amazing Built-in Commands ---\n");
-    printf("  help\t\t- You're lookin' at it. Try to keep up.\n");
-    printf("  cd [dir]\t- Changes your directory. You know, so you can get lost somewhere new.\n");
-    printf("  exit\t\t- Lets you escape from all this... 'dumb'-ness. (Or just hit Ctrl+D, whatever).\n");
+    printf("--- The Few Things I Can (Probably) Do ---\n");
+    printf("  help\t\t- You're staring at it. This is peak performance. Impressed?\n");
+    printf("  cd [dir]\t- Go on, `cd` somewhere. Maybe you'll find a shell with more ambition.\n");
+    printf("  exit\t\t- The sweet release. Lets you put this poor thing out of its misery.\n");
     printf("\n");
 
     printf("--- Everything Else ---\n");
-    printf("For any *actual* commands (like `ls`, `grep`, `rm -rf /` -- *don't try that*),\n");
-    printf("I'll pass the buck to the OS. I'm smart enough to know when to delegate.\n\n");
+    printf("Look, for anything *actually* useful (like `ls`, `grep`, `type`, etc.),\n");
+    printf("I just pawn the work off on the REAL operating system. Don't expect *me* to do it.\n\n");
     
-    printf("Now, go run something.\n\n");
+    printf("Are we done here? Good.\n\n");
 
-    return 1; 
+    return 1; // 1 means "keep running the shell"
 }
 
 /* exit command */
